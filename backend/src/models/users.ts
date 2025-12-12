a51d6d0022a6c82ea5aa32634476935437260cf3
alexandresilvestri <airtontunni@gmail.com>
2025-12-12 11:08:49 -0300

    create user tables to model users permissions

--- backend/src/models/users.ts
+++ backend/src/models/users.ts
@@ -2,6 +2,25 @@ export type User = {
   id: string
   email: string
   passwordHash: string
+  userType: UserTypes
+}
+
+export type UserTypes = {
+  id: string
+  name: string
+  description?: string
+  permission: Permission[]
+}
+
+export type Permission = {
+  id: string
+  name: string
+  module: string
+}
+
+export type TypeUserPermissions = {
+  typeUserId: string
+  permissionId: string
 }
 
 export type UserResponse = {
