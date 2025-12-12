6dd1e3e9be1bce1f9cddd9960a6ea9bea6ca1832
alexandresilvestri <airtontunni@gmail.com>
2025-12-12 11:10:01 -0300

    migration to create user_types table

---
+++ backend/src/database/migrations/20251212135834_create_type_user_table.ts
@@ -0,0 +1,17 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+
+  return knex.schema.createTable('user_types', (table) => {
+    table
+      .uuid('id', { primaryKey: true })
+      .defaultTo(knex.raw('uuid_generate_v4()'))
+    table.string('name').notNullable().unique()
+    table.timestamps(true, true)
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('user_types')
+}
