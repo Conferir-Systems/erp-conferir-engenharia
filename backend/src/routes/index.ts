41b47a85b61e378a5ca9aa06cb2300c197b13719
AS-Gaspar <airtontunni@gmail.com>
2025-12-31 16:32:47 -0300

    sync

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,19 +1,23 @@
 import express from 'express'
 import authRoutes from './auth'
+import { authenticate } from '../middleware/auth'
+import { validate } from '../validation/middleware'
+import { createUserSchema } from '../validation/schemas/users'
+import { createUserHandler } from '../controllers/users'
 import usersRoutes from './users'
 import userTypesRoutes from './userTypes'
 import worksRoutes from './works'
 import suppliersRoutes from './suppliers'
-// import { authenticate } from '../middleware/auth'
+import { getUserTypeSchema } from '../validation/schemas/userTypes'
+import { getUserTypeHandler } from '../controllers/userTypes'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
-router.post('/users', usersRoutes)
-router.get('/user-types', userTypesRoutes)
+router.post('/users', validate(createUserSchema), createUserHandler)
+router.get('/user-types', validate(getUserTypeSchema), getUserTypeHandler)
 
-//  router.use(authenticate)
-
+router.use(authenticate)
 router.use(usersRoutes)
 router.use(userTypesRoutes)
 router.use(worksRoutes)
